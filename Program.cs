f87a6ea3066631e09968a93d26aeaac0a2e1ea91
BIG DON <BIG DON@MSI>
2024-12-16 12:47:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ASSIGNMENT_7_1_2
+{
+    internal class Program
+    {
+        static string MergeAlternately(string word1, string word2)
+        {
+            int i = 0; 
+            int j = 0;
+            string result = "";
+
+            //Alternate adding characters from both strings
+            while (i < word1.Length && j < word2.Length)
+            {
+                result += word1[i];
+                result += word2[j];
+                i++;
+                j++;
+            }
+
+            //Append remaining characters from word1 if any 
+            while (i < word1.Length)
+            {
+                result += word1[i];
+                i++;
+            }
+
+            //Append remaining characters from word2 if any
+            while (j < word2.Length)
+            {
+                result += word2[j];
+                j++;
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            //Example 1 
+            string word1 = "abc";
+            string word2 = "pqr";
+            Console.WriteLine(MergeAlternately(word1, word2));
+
+            //Example 2
+            word1 = "ab";
+            word2 = "pqrs";
+            Console.WriteLine(MergeAlternately(word1, word2));
+        }
+    }
+}
